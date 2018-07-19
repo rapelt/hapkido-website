9d6bdc1a1c86f118ba3685a5c47599687d7f3898
Rebekah Apelt <rebekah_apelt@thoughtworks.com>
2018-07-19 10:35:16 +1000

    Self made side nav

--- src/app/shared-components/sidenav/sidenav.component.ts
+++ src/app/shared-components/sidenav/sidenav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {SidenavService} from '../../services/sidenav.service';
 
 @Component({
   selector: 'app-sidenav',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidenavComponent implements OnInit {
 
-  constructor() { }
+  constructor(private sideNavService: SidenavService) { }
 
   ngOnInit() {
+  }
+
+  close() {
+    this.sideNavService.sideNav.next();
   }
 
 }
